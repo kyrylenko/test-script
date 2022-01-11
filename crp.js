63ca3ad5b69901b34953f7ae867d46f392e84099
Pavlo <pavlo@id-ward.com>
2022-01-11 15:21:03 +0200

    correct

--- crp.js
+++ crp.js
@@ -1,19 +1,16 @@
-console.log('Hello from CRP');
-window.crp = { crp: 'hello' }
-
-    (() => {
-        ((a, b) => {
-            if ("undefined" != typeof module && module.exports)
-                return module.exports = b();
-            return "function" == typeof define && define.amd ? void define([], () => a.CrpMe = b()) : a.CrpMe = b()
-        }
-        )(this, () => {
-            let a = {
-                startStopTimes: {},
-                idleTimeoutMs: 30000
-            };
-            return a
-        }
-        )
+(() => {
+    ((a, b) => {
+        if ("undefined" != typeof module && module.exports)
+            return module.exports = b();
+        return "function" == typeof define && define.amd ? void define([], () => a.CrpMe = b()) : a.CrpMe = b()
     }
-    ).call(this);
+    )(this, () => {
+        let a = {
+            startStopTimes: {},
+            idleTimeoutMs: 30000
+        };
+        return a
+    }
+    )
+}
+).call(this);
