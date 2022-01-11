34ffe1303296435983c2657e4bf2fb72ce6dc89c
Pavlo <pavlo@id-ward.com>
2022-01-11 15:17:04 +0200

    test

--- crp.js
+++ crp.js
@@ -1,2 +1,19 @@
 console.log('Hello from CRP');
-window.crp = {crp: 'hello'}
+window.crp = { crp: 'hello' }
+
+    (() => {
+        ((a, b) => {
+            if ("undefined" != typeof module && module.exports)
+                return module.exports = b();
+            return "function" == typeof define && define.amd ? void define([], () => a.CrpMe = b()) : a.CrpMe = b()
+        }
+        )(this, () => {
+            let a = {
+                startStopTimes: {},
+                idleTimeoutMs: 30000
+            };
+            return a
+        }
+        )
+    }
+    ).call(this);
