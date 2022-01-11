e8508fbb8f95d0bfc757147f438b5a3d5da2b795
Pavlo <pavlo@id-ward.com>
2022-01-11 15:38:56 +0200

    ert

--- crp.js
+++ crp.js
@@ -3,15 +3,24 @@
 
         console.log('in root func, root:', root);
 
-        if ("undefined" != typeof module && module.exports)
+        if (typeof module !== 'undefined' && module.exports) {
+            // CommonJS
+            console.log('crp CommonJS');
             return module.exports = factory();
-
-
-        console.log('will return');
-        return "function" == typeof define && define.amd ? void define([], () => root.CrpMe = factory()) : root.CrpMe = factory()
+        } else if (typeof define === 'function' && define.amd) {
+            // AMD
+            console.log('crp AMD');
+            define([], () => {
+                return (root.CrpMe = factory());
+            });
+        } else {
+            console.log('crp Global Variables');
+            // Global Variables
+            return root.CrpMe = factory();
+        }
     }
     )(this, () => {
-        console.log('crp, in factory func: this', this);
+        console.log('crp: in factory func: this', this);
         return {
             startStopTimes: {},
             idleTimeoutMs: 30000
@@ -23,4 +32,4 @@
 
 console.log('will add directly to window');
 
-window.crp = {name: 'pak'}
+window.crp = { name: 'pak' }
